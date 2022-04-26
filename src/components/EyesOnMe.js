b255c7281f58215b7be1acffdeb816502d4c8a6b
Samantha Crespo <samkcrespo@gmail.com>
2022-04-26 16:43:34 -0700

    commit tree f011b2b33e48cc8f0fd03463d1a5a215a998f08e

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
+import React from "react";
+
+function EyesOnMe() {
+  function onFocus(event) {
+    console.log("Good!");
+  }
+  function onBlur(event) {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <button onFocus={onFocus} onBlur={onBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
+
 // Code EyesOnMe Component Here
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  function onChange(event) {
+    console.log("Entering password...");
+  }
+  return <input onChange={onChange} type="password" />;
+}
+
+export default Keypad; // Code Keypad Component Here
